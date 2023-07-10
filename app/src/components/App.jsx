9acff0034bc260cf8792afd610e5085e5f3afed1
Shantanu Sutar <shantanusutar17@gmail.com>
2023-07-11 01:43:12 +0530

    choosing an option and displaying the correct answer

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -10,6 +10,7 @@ const initialState = {
   questions: [],
   status: "loading", //  loading | error | ready | active | finished
   index: 0, // index of the current question
+  answer: null, // the answer to the current question
 }; // initial state of the app is an empty array of questions and a status of loading
 
 function reducer(state, action) {
@@ -27,13 +28,18 @@ function reducer(state, action) {
       }; // if the data fails to load, the status is error
     case "start":
       return { ...state, status: "active" }; // when the game starts, the status is active
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   ); // useReducer is a hook that allows us to manage state with a reducer function
@@ -58,7 +64,13 @@ function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {/* if the status is ready, we show the start screen */}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
         {/* if the status is active, we show the question */}
       </Main>
     </div>
--- app/src/components/Options.jsx
+++ app/src/components/Options.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-const Options = ({ question }) => {
+const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null; // if the answer is not null, it means the user has answered the question
   return (
     <div className="options">
-      {question.options.map((option) => {
+      {question.options.map((option, index) => {
         return (
-          <button className="btn btn-option" key={option}>
+          <button
+            className={`btn btn-option ${index === answer ? "answer" : ""} ${
+              hasAnswered
+                ? index === question.correctOption
+                  ? "correct"
+                  : "wrong"
+                : ""
+            }`} // if the answer is correct, we add the correct class, if the answer is wrong, we add the wrong class and if the answer is null, we add the answer class
+            key={option}
+            disabled={hasAnswered}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })} // when the user clicks on an option, we dispatch the newAnswer action with the index of the option as payload
+          >
             {option}
           </button>
         );
